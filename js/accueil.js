a1104a8cc14f67e77751f07bd1165623ffc61f03
Eric <70970042+devevh@users.noreply.github.com>
2023-04-05 21:18:26 +0200

    Add files via upload
    mise à jour avec la bonne version

--- js/accueil.js
+++ js/accueil.js
@@ -1,13 +1,12 @@
 function furtivite(id) {
   let x = document.getElementById(id);
   let y = document.getElementById('gererAffichage_'+id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-    y.className = "fa fa-arrow-up";
-	//y.innerHTML = "v";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
+  if (x.className.indexOf("w3-hide") >= 0) {
+      x.className = x.className.replace("w3-hide","w3-show");
+      y.className = "fa fa-arrow-up";
+  }
+  else {
+	x.className = x.className.replace("w3-show", "w3-hide");
     y.className = "fa fa-arrow-down";
-	//y.innerHTML = "&gt;";
   }
 }
