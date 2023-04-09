44186c4b5f217e522c0c29f993af7a4633f4da45
Eric <70970042+devevh@users.noreply.github.com>
2023-04-09 21:48:20 +0200

    Delete _contenu_distrib.js

--- js/_contenu_distrib.js
+++
@@ -1,6 +0,0 @@
-function refresh() {
-	location.reload();
-}
-//clic sur reload
-document.getElementById('refreshCC').addEventListener('click', refresh);
-document.getElementById('refreshVF').addEventListener('click', refresh);
