f73c52c5d11270a6ccad591c22e8acf61da55666
Eric <70970042+devevh@users.noreply.github.com>
2023-04-09 21:51:16 +0200

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-//clic sur se connecter pour afficher la modale du formulaire de connexion
-document.getElementById('menuConnecter').addEventListener('click',function(){
-	document.getElementById('modaleConnecter').style.display='block';
-});
-//clic sur le x pour fermer la modale de connexion
-document.getElementById('fermerConnecter').addEventListener('click',function(){
-	document.getElementById('modaleConnecter').style.display='none';
-});
