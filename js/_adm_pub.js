abf1026c59fdc3f71c800e12036f84371760165a
Eric <70970042+devevh@users.noreply.github.com>
2023-04-09 21:45:38 +0200

    Delete _adm_pub.js

--- js/_adm_pub.js
+++
@@ -1,8 +0,0 @@
-//clic sur se connecter pour afficher la modale du formulaire de connexion
-document.getElementByClassName('imgpub').addEventListener('click',function(){
-	document.getElementById('modaleImagePub').style.display='block';
-});
-//clic sur le x pour fermer la modale de connexion
-document.getElementById('fermerPub').addEventListener('click',function(){
-	document.getElementById('modaleImagePub').style.display='none';
-});
