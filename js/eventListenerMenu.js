7f2c92caa9c0afa4d66ed858beda28cfbb9ecd73
Eric <70970042+devevh@users.noreply.github.com>
2023-04-09 21:50:58 +0200

    Delete eventListenerMenu.js

--- js/eventListenerMenu.js
+++
@@ -1,4 +0,0 @@
-//clic sur le sandwich pour ouvrir le menu
-document.getElementById('ouvrirmenu').addEventListener('click', w3_open());
-//clic sur la croix pour fermer le menu
-document.getElementById('fermermenu').addEventListener('click', w3_close());
